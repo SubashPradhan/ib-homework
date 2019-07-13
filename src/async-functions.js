ca7fa6d02baa3fe3e292231c1a5555f9c70d8512
Subash Pradahan <suprowilde5@gmail.com>
2019-07-13 15:09:36 +0200

    6 out of 7 done

--- src/async-functions.js
+++ src/async-functions.js
@@ -1,20 +1,29 @@
+
+// resolve with only arrays of url for the resident of tatooline residents
 const getTatooineResidents = () => {
-const superagent = require('superagent')
+    const superagent = require('superagent')
     return superagent
-    .get('https://swapi.co/api/planets/1/')
-    
+        .get('https://swapi.co/api/planets/1/')
+        .then(res => res.body.residents)
+        .catch(error => `Please Try again later !!! ${error.message}`)
+
 }
 
+// Creating new Promise that checks the string validation
+const promiseMeAString = (checkIfIsString) => {
+    return new Promise((resolve, reject) => {
+        if (typeof checkIfIsString === 'string') {
+            resolve('You kept the Promise!')
+        } else if (typeof checkIfIsString === null) {
+            reject('You have failed me!')
+        }
+    })
+}
+
+setTimeout(promiseMeAString, 1000) // trying to set timeout for last error
 
 
-
-
-
-
-
-
-
-module.exports = {   
+module.exports = {
     getTatooineResidents,
     promiseMeAString
-    }
+}
